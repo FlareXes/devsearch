f4e05e5e143235f4eb047171a481ee438f2aa0ea
FlareXes <70066527+FlareXes@users.noreply.github.com>
2025-01-31 21:18:26 +0530

    fest: integrate models with templates

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Project
+
 
 def projects(request):
-    msg = "hello world"
-    context = {
-        "msg": msg
-    }
-    return render(request, 'projects/index.html', context=context)
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/projects.html", context=context)
+
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+
+    context = {"project": project, "tags": tags}
+    return render(request, "projects/single-project.html", context=context)
